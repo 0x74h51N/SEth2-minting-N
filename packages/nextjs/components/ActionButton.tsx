ee8fc9062d5b2211ed56993a9a557aabb33c6371
Tahsin Önemli <129261873+RicochetBiscuit@users.noreply.github.com>
2023-09-26 17:25:30 +0300

    refSetup warnColor Changes

--- packages/nextjs/components/ActionButton.tsx
+++ packages/nextjs/components/ActionButton.tsx
@@ -18,9 +18,11 @@ const ActionButton = (
   const amountInputRefB = useRef<HTMLInputElement | null>(null);
 
   const handleAddrChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    handleInputError(addressInputRef, undefined, false);
     setAddr(event.target.value);
   };
   const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    handleInputError(showAddressInput ? amountInputRef : amountInputRefB, undefined, false);
     showAddressInput ? setAmount(event.target.value) : setAmountB(event.target.value);
   };
   const { writeAsync: asyncMint } = useScaffoldContractWrite({
@@ -38,22 +40,17 @@ const ActionButton = (
     console.log(`${functionName} ${contractName} Token!`);
     if (showAddressInput) {
       if (!/^0x[0-9a-fA-F]{40}$/.test(addr.trim())) {
-        handleInputError(addressInputRef, "INVALID_ADDRESS");
-        return;
-      } else if (amount !== "" && amount !== "0") {
-        handleInputError(amountInputRef, "AMOUNT_EMPTY");
-        return;
+        return handleInputError(addressInputRef, "INVALID_ADDRESS");
+      } else if (amount == "" || amount == "0") {
+        return handleInputError(amountInputRef, "AMOUNT_EMPTY");
       } else if (amount.includes(",")) {
-        handleInputError(amountInputRef, "DECIMAL_SEPARATOR");
-        return;
+        return handleInputError(amountInputRef, "DECIMAL_SEPARATOR");
       }
     } else {
-      if (amountB !== "" && amountB !== "0") {
-        handleInputError(amountInputRefB, "AMOUNT_EMPTY");
-        return;
+      if (amountB == "" || amountB == "0") {
+        return handleInputError(amountInputRefB, "AMOUNT_EMPTY");
       } else if (amountB.includes(",")) {
-        handleInputError(amountInputRefB, "DECIMAL_SEPARATOR");
-        return;
+        return handleInputError(amountInputRefB, "DECIMAL_SEPARATOR");
       }
     }
     const arg = parseEther(amount);
@@ -74,6 +71,40 @@ const ActionButton = (
               <input placeholder="Wallet Address" ref={addressInputRef} className="input" onChange={handleAddrChange} />
             </>
           )}
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer accumsan pulvinar lectus. Aenean dapibus,
+            tortor nec feugiat feugiat, mauris magna dictum felis, ut semper nisl urna sit amet odio. Etiam sit amet
+            ante ultricies, gravida ante nec, suscipit ipsum. Pellentesque non turpis vestibulum tortor facilisis dictum
+            eget at tellus. Ut tempor vestibulum est, sed iaculis enim blandit nec. Sed suscipit nec libero nec
+            vehicula. Praesent laoreet purus id ligula sagittis accumsan. Vestibulum erat tortor, ullamcorper eu lacus
+            et, auctor facilisis tortor. Praesent a metus commodo, pretium diam ac, convallis diam. Etiam commodo lorem
+            ut venenatis suscipit. Nunc finibus laoreet rhoncus. Nunc rutrum vel justo sagittis rhoncus. Nunc laoreet
+            dignissim ipsum a condimentum. Etiam at leo eget massa placerat posuere nec et neque. Nam at mi ut eros
+            luctus commodo. Nam quam urna, dapibus sed odio id, luctus dignissim est. Ut sagittis dolor justo, in
+            placerat sapien rhoncus vel. Vivamus consectetur orci id arcu rutrum, nec tincidunt nulla facilisis.
+            Pellentesque vitae blandit neque, sed condimentum neque. Donec maximus ultricies sagittis. In dictum neque
+            ipsum, vel finibus sem auctor eu. Suspendisse et nisl ut dolor auctor suscipit nec ut ligula. Donec sagittis
+            lacus tellus, vitae commodo nisi laoreet quis. In faucibus consequat maximus. Pellentesque laoreet eu lacus
+            congue placerat. Cras rhoncus id neque sit amet aliquam. Phasellus ornare quam at ligula viverra, eu
+            sagittis dui placerat. Proin convallis mollis ligula, ac pulvinar sapien viverra vitae. Donec pellentesque
+            metus elit, at laoreet erat mattis quis. Maecenas ut dui id risus consectetur scelerisque. Integer non
+            mauris ullamcorper, imperdiet eros sit amet, fringilla erat. Donec id tortor vel massa blandit molestie ut
+            quis tortor. Praesent sed leo at enim porttitor bibendum sed vitae nisl. Suspendisse eget mollis ex. Integer
+            porta felis vel dui hendrerit, id sagittis augue facilisis. Nam pellentesque mauris ac nisi finibus lacinia.
+            Pellentesque aliquam dapibus convallis. Fusce dignissim maximus erat, non gravida odio tempus quis. Donec
+            lobortis magna in erat varius, non posuere enim maximus. Morbi sed nibh sagittis, dictum velit vel, feugiat
+            lorem. Praesent vitae eleifend sapien. Vestibulum aliquam, ipsum non venenatis scelerisque, sem nulla
+            posuere lacus, vitae rhoncus velit risus et felis. Proin elit neque, vestibulum vel volutpat vitae, ornare
+            ut libero. Curabitur faucibus dolor a nibh bibendum porttitor. Nullam ac nulla et nisl finibus accumsan
+            dignissim sed sem. Aenean quis iaculis urna. Aenean efficitur justo vel augue malesuada condimentum. Nunc ac
+            rutrum eros, quis eleifend orci. Curabitur luctus consectetur diam sed convallis. Suspendisse ut aliquet
+            lorem. Cras venenatis ultrices libero ac volutpat. Integer vestibulum tempor egestas. Sed accumsan augue sed
+            maximus scelerisque. Vestibulum ullamcorper, purus laoreet ultrices cursus, eros leo finibus massa, non
+            malesuada ligula lacus sit amet augue. Curabitur magna nulla, rhoncus non nibh ut, iaculis hendrerit mauris.
+            Fusce imperdiet ligula venenatis, placerat dolor in, dapibus ipsum. Vestibulum arcu velit, malesuada nec
+            dictum eu, egestas ut lorem. Quisque iaculis iaculis turpis, ut gravida ex aliquam sit amet. Cras pharetra
+            commodo ullamcorper. Ut ligula metus, luctus eu imperdiet vel, laoreet vel elit.
+          </p>
           <p className="text">Amount:</p>
           <input
             placeholder={`${contractName === "NVMToken" ? "NVM" : "NNN"} Token Amount`}
--- packages/nextjs/utils/errorHandling.ts
+++ packages/nextjs/utils/errorHandling.ts
@@ -1,4 +1,3 @@
-// errorHandling.ts
 import { notification } from "~~/utils/scaffold-eth";
 
 const ERROR_MESSAGES = {
@@ -6,25 +5,25 @@ const ERROR_MESSAGES = {
   AMOUNT_EMPTY: "Amount field cannot be empty.",
   DECIMAL_SEPARATOR: 'Use dot "." for decimal separator.',
 };
-const ERROR_RESET_DELAY = 2000;
 export const handleInputError = (
   inputRef: React.RefObject<HTMLInputElement> | null,
   errorKey?: keyof typeof ERROR_MESSAGES,
+  errorTrue = true,
 ) => {
-  if (errorKey) {
+  if (errorKey && errorTrue) {
     const errorMessage = ERROR_MESSAGES[errorKey];
     notification.error(errorMessage);
 
     if (inputRef && inputRef.current) {
       const currentInput = inputRef.current;
-      currentInput.focus();
       currentInput.classList.add("red-border");
-
+      currentInput.scrollIntoView({ behavior: "smooth", block: "center" });
       setTimeout(() => {
-        if (currentInput) {
-          currentInput.classList.remove("red-border");
-        }
-      }, ERROR_RESET_DELAY);
+        currentInput.focus();
+      }, 500);
     }
+  } else if (inputRef && inputRef.current) {
+    const currentInput = inputRef.current;
+    currentInput.classList.remove("red-border");
   }
 };
