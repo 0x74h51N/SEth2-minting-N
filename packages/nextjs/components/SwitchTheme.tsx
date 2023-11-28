9e22eca2082fe83392fb638f54e7e0a09ae7e630
Tahsin Önemli <129261873+RicochetBiscuit@users.noreply.github.com>
2023-11-28 11:35:03 +0300

    theme

--- packages/nextjs/components/SwitchTheme.tsx
+++ packages/nextjs/components/SwitchTheme.tsx
@@ -1,15 +1,14 @@
 import { useEffect } from "react";
-import { useDarkMode, useIsMounted } from "usehooks-ts";
+import { useDarkMode } from "usehooks-ts";
 import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
 
 export const SwitchTheme = ({ className }: { className?: string }) => {
   const { isDarkMode, toggle } = useDarkMode();
-  const isMounted = useIsMounted();
 
   useEffect(() => {
     const body = document.body;
-    body.setAttribute("data-theme", isDarkMode ? "scaffoldEthDark" : "scaffoldEth");
-  }, [isDarkMode]);
+    body.setAttribute("data-theme", isDarkMode ? "tokenDark" : "tokenLight");
+  }, [isDarkMode, toggle]);
 
   return (
     <div className={`flex space-x-2 text-sm ${className}`}>
@@ -20,12 +19,11 @@ export const SwitchTheme = ({ className }: { className?: string }) => {
         onChange={toggle}
         checked={isDarkMode}
       />
-      {isMounted() && (
-        <label htmlFor="theme-toggle" className={`swap swap-rotate ${!isDarkMode ? "swap-active" : ""}`}>
-          <SunIcon className="swap-on h-5 w-5" />
-          <MoonIcon className="swap-off h-5 w-5" />
-        </label>
-      )}
+
+      <label htmlFor="theme-toggle" className={`swap swap-rotate ${!isDarkMode ? "swap-active" : ""}`}>
+        <SunIcon className="swap-on h-5 w-5" />
+        <MoonIcon className="swap-off h-5 w-5" />
+      </label>
     </div>
   );
 };
